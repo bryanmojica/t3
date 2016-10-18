0a2b06f14803eceb51e1f4efea5a0724f7af15c5
bryanmojica <bryan@trestian.com>
2016-10-18 12:11:34 -0700

    implement player turns and toggle X/O output accordingly

--- gameLogic.js
+++ gameLogic.js
@@ -8,13 +8,16 @@ var gameBoard = [
  [0, 0, 0],
 ];
 
+var playerOneTurn = true;
+
 var spaces = document.getElementsByClassName('space');
 
 window.onload = function () {
   for ( var i = 0; i < spaces.length; i ++ ) {
     console.log('looping on this', spaces[i]);
     spaces[i].addEventListener('click', function () {
-      this.innerHTML = 'X';
+      playerOneTurn ? this.innerHTML = 'X' : this.innerHTML = 'O';
+      playerOneTurn = !playerOneTurn;
     }); 
   }
 };
