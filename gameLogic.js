dadd771404e62a4c8c856b4a14fa13da3b1c5445
bryanmojica <bryan@trestian.com>
2016-10-18 12:30:40 -0700

    implement pseudocode for checking and registering game win

--- gameLogic.js
+++ gameLogic.js
@@ -1,7 +1,3 @@
-
-
-
-// display player move
 var gameBoard = [
  [0, 0, 0],
  [0, 0, 0],
@@ -14,10 +10,34 @@ var spaces = document.getElementsByClassName('space');
 
 window.onload = function () {
   for ( var i = 0; i < spaces.length; i ++ ) {
-    console.log('looping on this', spaces[i]);
     spaces[i].addEventListener('click', function () {
-      playerOneTurn ? this.innerHTML = 'X' : this.innerHTML = 'O';
-      playerOneTurn = !playerOneTurn;
+      if ( !this.innerHTML ) {
+        playerOneTurn ? this.innerHTML = 'X' : this.innerHTML = 'O';
+        console.log('clicked', this);
+        playerOneTurn = !playerOneTurn;
+      }
     }); 
   }
 };
+
+var count = 0;
+
+// map the gameboard spaces to gameBoard matrix
+  // onClick change matrix position to 1 or 2, dependent on given pleyer's turn
+
+
+var checkHorizontal = function () {
+  // tally number of 1 and 2 instances, horizontally across row
+    // if number of instances === 3, endGame and increment player X win
+};
+
+var checkVertical = function () {
+  // tally number of 1 and 2 instances, vertically along columns
+    // if number of instances === 3, endGame and increment player X win
+};
+
+var checkDiagonal = function () {
+  // tally number of 1 and 2 instances, diagonally
+    // if number of instances === 3, endGame and increment player X win
+};
+
